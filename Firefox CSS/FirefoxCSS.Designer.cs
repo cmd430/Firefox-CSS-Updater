9f06831e887c2c25cf8216760cccf05582e4b454
cmd430 <cmd430@gmail.com>
2022-10-22 23:46:38 +1300

    Add project files.

---
+++ Firefox CSS/FirefoxCSS.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Firefox_CSS
+{
+    partial class FirefoxCSS
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FirefoxCSS));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_currentVersion = new System.Windows.Forms.TextBox();
+            this.textBox_latestVersion = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox_user = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Current Version:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Latest Version: ";
+            // 
+            // textBox_currentVersion
+            // 
+            this.textBox_currentVersion.Enabled = false;
+            this.textBox_currentVersion.Location = new System.Drawing.Point(100, 6);
+            this.textBox_currentVersion.Name = "textBox_currentVersion";
+            this.textBox_currentVersion.ReadOnly = true;
+            this.textBox_currentVersion.Size = new System.Drawing.Size(262, 20);
+            this.textBox_currentVersion.TabIndex = 3;
+            // 
+            // textBox_latestVersion
+            // 
+            this.textBox_latestVersion.Enabled = false;
+            this.textBox_latestVersion.Location = new System.Drawing.Point(100, 32);
+            this.textBox_latestVersion.Name = "textBox_latestVersion";
+            this.textBox_latestVersion.ReadOnly = true;
+            this.textBox_latestVersion.Size = new System.Drawing.Size(262, 20);
+            this.textBox_latestVersion.TabIndex = 4;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 16);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(341, 352);
+            this.flowLayoutPanel1.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox_user);
+            this.groupBox1.Controls.Add(this.flowLayoutPanel1);
+            this.groupBox1.Location = new System.Drawing.Point(15, 67);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(347, 371);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Settings";
+            // 
+            // textBox_user
+            // 
+            this.textBox_user.Location = new System.Drawing.Point(6, 236);
+            this.textBox_user.Multiline = true;
+            this.textBox_user.Name = "textBox_user";
+            this.textBox_user.Size = new System.Drawing.Size(335, 130);
+            this.textBox_user.TabIndex = 0;
+            this.textBox_user.Visible = false;
+            // 
+            // FirefoxCSS
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox_latestVersion);
+            this.Controls.Add(this.textBox_currentVersion);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FirefoxCSS";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Firefox CSS";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_currentVersion;
+        private System.Windows.Forms.TextBox textBox_latestVersion;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox_user;
+    }
+}
+
---
+++ Firefox CSS/FirefoxCSS.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Net;
+
+namespace Firefox_CSS
+{
+    public partial class FirefoxCSS : Form
+    {
+        public FirefoxCSS()
+        {
+            InitializeComponent();
+        }
+
+        private static readonly string AppData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        private static readonly string FirefoxDir = Path.Combine(AppData, "Mozilla", "Firefox");
+        private static readonly string ProfilesDir = Path.Combine(FirefoxDir, "Profiles");
+        private static readonly string VersionFile = Path.Combine(FirefoxDir, "css.version");
+       
+        private string ProfileDir = null;
+        private string userChromePath = null;
+        private string userPath = null;
+        private string CurrentVersion = "";
+        private string LatestVersion = "";
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            string profilesText = File.ReadAllText(Path.Combine(FirefoxDir, "profiles.ini"));
+            string profile = Regex.Match(profilesText, @"Default=Profiles/([\w\d.-]+)").Groups[1].Value;
+
+            ProfileDir = Path.Combine(ProfilesDir, profile);
+            userChromePath = Path.Combine(ProfileDir, "chrome", "userChrome.css");
+            userPath = Path.Combine(ProfileDir, "user.js");
+
+            if (File.Exists(VersionFile)) CurrentVersion = File.ReadAllText(VersionFile);
+
+            using (WebClient client = new WebClient())
+            {
+                client.Headers.Add("User-Agent", "c#");
+                string GitHubJson = client.DownloadString("https://api.github.com/repos/cmd430/FirefoxCSS/commits/master");
+                LatestVersion = Regex.Match(GitHubJson, @"""sha"":""(.{40})"",").Groups[1].Value;
+            }
+
+            textBox_currentVersion.Text = CurrentVersion;
+            textBox_latestVersion.Text = LatestVersion;
+
+            if (CurrentVersion != LatestVersion)
+            {
+                if (MessageBox.Show("Do you want to install the latest update?", "Update", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    using (WebClient client = new WebClient())
+                    {
+                        client.Headers.Add("User-Agent", "c#");
+                        string userChrome = client.DownloadString("https://raw.githubusercontent.com/cmd430/FirefoxCSS/master/dist/chrome/userChrome.css");
+                        string user = client.DownloadString("https://raw.githubusercontent.com/cmd430/FirefoxCSS/master/dist/user.js");
+
+                        File.WriteAllText(userChromePath, userChrome);
+                        File.WriteAllText(userPath, user);
+                        File.WriteAllText(VersionFile, LatestVersion);
+
+                        CurrentVersion = LatestVersion;
+                        textBox_currentVersion.Text = CurrentVersion;
+                    }
+                }
+            }
+
+            if (File.Exists(userPath))
+            {
+                IEnumerable<string> userOptions = File.ReadLines(userPath);
+                bool foundUserConfigurable = false;
+
+                foreach (string userOption in userOptions)
+                {
+                    textBox_user.AppendText(userOption);
+                    textBox_user.AppendText("\r\n");
+
+                    if (userOption == "//userChrome Prefs")
+                    {
+                        foundUserConfigurable = true;
+                        continue;
+                    }
+
+                    if (foundUserConfigurable == false) continue;
+
+                    GroupCollection groups = Regex.Match(userOption, @"user_pref\(""userChrome\.(\w+)"", (true|false)\);").Groups;
+                    CheckBox box = new CheckBox
+                    {
+                        Tag = userOption,
+                        Text = groups[1].Value,
+                        AutoSize = true,
+                        Checked = groups[2].Value == "true" ? true : false,
+                        Parent = flowLayoutPanel1
+                    };
+
+                    box.CheckedChanged += checkBox_CheckedChanged;
+                    flowLayoutPanel1.Controls.Add(box);
+                }
+                textBox_user.Text = textBox_user.Text.Trim();
+            }
+        }
+
+        private void checkBox_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckBox checkbox = (CheckBox)sender;
+            GroupCollection groups = Regex.Match(checkbox.Tag.ToString(), @"user_pref\(""userChrome\.(\w+)"", (true|false)\);").Groups;
+
+            string oldValue = String.Concat("user_pref(\"userChrome.", groups[1].Value, "\", ", groups[2].Value, ");");
+            string newValue = String.Concat("user_pref(\"userChrome.", groups[1].Value, "\", ", checkbox.Checked ? "true" : "false", ");");
+
+            textBox_user.Text = textBox_user.Text.Replace(oldValue, newValue);
+            checkbox.Tag = newValue;
+
+            File.WriteAllText(userPath, textBox_user.Text);
+        }
+    }
+}
